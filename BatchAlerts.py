a7350bc801c4018b86aa3a3a735bb68ca47acf44
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-03-28 11:29:34 -0500

    Create BatchAlerts.py

---
+++ BatchAlerts.py
@@ -0,0 +1,6 @@
+df_enriched.repartition(4).write.format("mongodb") \
+    .option("batchSize", 1000) \
+    .mode("append") \
+    .option("database", "northwind") \
+    .option("collection", "orders_full") \
+    .save()
